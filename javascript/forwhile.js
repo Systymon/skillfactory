664e4c333f1f6723dc43e8ab72bd2b0eae564dbe
Tymon <systymon@gmail.com>
2021-09-27 01:53:45 +0500

    new

---
+++ javascript/forwhile.js
@@ -0,0 +1,5 @@
+let array = ['Boby', 'Ivan', 'Sveta', 'Katya']
+for (let i = 1; i < array.length; i++) {
+  if (array[0] != array[i]) break
+}
+console.log('false')
---
+++ javascript/number.js
@@ -0,0 +1,11 @@
+let x = prompt()
+x = +x
+if (x) {
+  if (x % 2 == 0) {
+    alert('Четное')
+  } else {
+    alert('Не четное')
+  }
+} else {
+  alert('Упс Что-то пошло не так!')
+}
