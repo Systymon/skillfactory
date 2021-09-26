31abd66bd975f64401ad0844b788454bb7f83b55
Tymon <systymon@gmail.com>
2021-09-27 01:57:00 +0500

    new

--- javascript/forwhile.js
+++
@@ -1,5 +0,0 @@
-let array = ['Boby', 'Ivan', 'Sveta', 'Katya']
-for (let i = 1; i < array.length; i++) {
-  if (array[0] != array[i]) break
-}
-console.log('false')
--- javascript/helloworld.js
+++
@@ -1,6 +0,0 @@
-let number = [1, 2, 3, 4]
-console.log(number.length)
-console.log(number[0])
-console.log(number[1])
-console.log(number[2])
-console.log(number[3])
