bcdc03821eda2ed57e38ff55907baf771ec3d206
Systymon <86165505+Systymon@users.noreply.github.com>
2023-06-17 01:38:47 +0500

    Delete javascript directory

--- javascript/arow.js
+++
@@ -1,4 +0,0 @@
-const arow = (x, n) => {
-  console.log(x ** n)
-}
-arow(2, 2)
--- javascript/map.js
+++
@@ -1,8 +0,0 @@
-let myMap = new Map([
-  ['apple', 'green'],
-  ['strawberry', 'red'],
-  ['blueberry', 'blue'],
-])
-for (let name of myMap.keys()) {
-  console.log(name + ' - ' + myMap.get(name))
-}
